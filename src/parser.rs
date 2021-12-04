331754dc6b57e144e78fe62fe46b966c8115a7c3
佐藤昭文 <akfm.sato@gmail.com>
2021-12-05 00:36:02 +0900

    add annotation, location struct

--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,33 @@
 extern crate wasm_bindgen;
 
+use std::cmp::{max, min};
 use wasm_bindgen::prelude::*;
+
+/// Location情報
+/// (start, end)で保持する
+/// ```
+/// let a = Location(start, end);
+/// ```
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct Location(i32, i32);
+
+impl Location {
+    fn merge(&self, target: Location) -> Location {
+        Location(min(self.0, target.0), max(self.1, target.1))
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct Annotation<T> {
+    value: T,
+    location: Location,
+}
+
+impl<T> Annotation<T> {
+    fn new(value: T, location: Location) -> Self {
+        Annotation { value, location }
+    }
+}
 
 // enum Token {
 //     OpenBrace, // `{`
@@ -17,6 +44,15 @@ pub fn greet(name: &str) -> String {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn location_merge() {
+        let location1 = Location(0, 1);
+        let location2 = Location(1, 10);
+        let merge_result = location1.merge(location2);
+        assert_eq!(merge_result.0, 0);
+        assert_eq!(merge_result.1, 10);
+    }
 
     #[test]
     fn greet_name() {
