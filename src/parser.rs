b7d29b148d761b1a46d35338f321b9e18c235fc7
佐藤昭文 <akfm.sato@gmail.com>
2021-12-05 00:19:53 +0900

    split parser.rs

---
+++ src/parser.rs
@@ -0,0 +1,25 @@
+extern crate wasm_bindgen;
+
+use wasm_bindgen::prelude::*;
+
+// enum Token {
+//     OpenBrace, // `{`
+//     CloseBrace, // `}`
+//     String(String),
+// }
+
+#[wasm_bindgen]
+pub fn greet(name: &str) -> String {
+    let message = format!("Hello, {}!!!", name);
+    message.to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn greet_name() {
+        assert_eq!(greet("world"), "Hello, world!!!");
+    }
+}
