478e1e708dd444f33b901e11699b95e0a305caca
佐藤昭文 <akfm.sato@gmail.com>
2021-12-05 23:05:27 +0900

    add token struct

--- src/parser.rs
+++ src/parser.rs
@@ -29,11 +29,90 @@ impl<T> Annotation<T> {
     }
 }
 
-// enum Token {
-//     OpenBrace, // `{`
-//     CloseBrace, // `}`
-//     String(String),
-// }
+enum TokenKind {
+    OpenBrace, // `{`
+    CloseBrace, // `}`
+    OpenBracket, // `[`
+    CloseBracket, // `]`
+    String(String),
+    Number(String), // 浮動少数誤差を扱わないため、String
+    Boolean(bool),
+    Null,
+    CommentLine(String),
+    CommentBlock(String),
+    Comma,
+    Colon,
+    WhiteSpaces(i32), // Length
+}
+
+type Token = Annotation<TokenKind>;
+
+impl Token {
+    fn open_brace(location: Location) -> Self {
+        Self::new(TokenKind::OpenBrace, location)
+    }
+
+    fn close_brace(location: Location) -> Self {
+        Self::new(TokenKind::CloseBrace, location)
+    }
+
+    fn open_bracket(location: Location) -> Self {
+        Self::new(TokenKind::OpenBracket, location)
+    }
+
+    fn close_bracket(location: Location) -> Self {
+        Self::new(TokenKind::CloseBracket, location)
+    }
+
+    fn string(value: &str, location: Location) -> Self {
+        Self::new(TokenKind::String(value.to_string()), location)
+    }
+
+    fn number(value: &str, location: Location) -> Self {
+        Self::new(TokenKind::Number(value.to_string()), location)
+    }
+
+    fn boolean(value: bool, location: Location) -> Self {
+        Self::new(TokenKind::Boolean(value), location)
+    }
+
+    fn null(location: Location) -> Self {
+        Self::new(TokenKind::Null, location)
+    }
+
+    fn comment_line(value: &str, location: Location) -> Self {
+        Self::new(TokenKind::CommentLine(value.to_string()), location)
+    }
+
+    fn comment_block(value: &str, location: Location) -> Self {
+        Self::new(TokenKind::CommentBlock(value.to_string()), location)
+    }
+
+    fn comma(location: Location) -> Self {
+        Self::new(TokenKind::Comma, location)
+    }
+
+    fn colon(location: Location) -> Self {
+        Self::new(TokenKind::Colon, location)
+    }
+
+    fn white_spaces(length: i32, location: Location) -> Self {
+        Self::new(TokenKind::WhiteSpaces(length), location)
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+enum LexerErrorKind {
+    InvalidChars(String),
+}
+
+type LexerError = Annotation<LexerErrorKind>;
+
+impl LexerError {
+    fn invalid_chars(chars: String, location: Location) -> Self {
+        Annotation::new(LexerErrorKind::InvalidChars(chars), location)
+    }
+}
 
 #[wasm_bindgen]
 pub fn greet(name: &str) -> String {
