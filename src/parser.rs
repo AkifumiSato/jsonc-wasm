bf68898c0299057a789faaf1a328517a0d7be938
佐藤昭文 <akfm.sato@gmail.com>
2021-12-19 22:56:45 +0900

    rename parse to scan

--- src/parser.rs
+++ src/parser.rs
@@ -29,33 +29,33 @@ impl<'a> Lexer<'a> {
                 '[' => tokens.push(Token::open_bracket(Location(index, index + 1))),
                 ']' => tokens.push(Token::close_bracket(Location(index, index + 1))),
                 '"' => {
-                    let token = self.parse_string_token()?;
+                    let token = self.scan_string_token()?;
                     tokens.push(token);
                 }
                 c if is_number_token_char(c) => {
-                    let token = self.parse_number_token(c)?;
+                    let token = self.scan_number_token(c)?;
                     tokens.push(token);
                 }
                 't' => {
-                    let token = self.parse_bool_token(true, index)?;
+                    let token = self.scan_bool_token(true, index)?;
                     tokens.push(token);
                 }
                 'f' => {
-                    let token = self.parse_bool_token(false, index)?;
+                    let token = self.scan_bool_token(false, index)?;
                     tokens.push(token);
                 }
                 'n' => {
-                    let token = self.parse_null_token(index)?;
+                    let token = self.scan_null_token(index)?;
                     tokens.push(token);
                 }
                 ':' => tokens.push(Token::colon(Location(index, index + 1))),
                 ',' => tokens.push(Token::comma(Location(index, index + 1))),
                 '/' => {
-                    let token = self.parse_comment_token()?;
+                    let token = self.scan_comment_token()?;
                     tokens.push(token);
                 }
                 ' ' => {
-                    let token = self.parse_whitespaces()?;
+                    let token = self.scan_whitespaces()?;
                     tokens.push(token);
                 }
                 // todo break(\n\r)
@@ -66,7 +66,7 @@ impl<'a> Lexer<'a> {
         Ok(tokens)
     }
 
-    fn parse_string_token(&mut self) -> Result<Token, LexerError> {
+    fn scan_string_token(&mut self) -> Result<Token, LexerError> {
         let mut value = String::new();
         let mut times = 0;
 
@@ -108,7 +108,7 @@ impl<'a> Lexer<'a> {
         Err(LexerError::not_exist_terminal_symbol())
     }
 
-    fn parse_number_token(&mut self, first: char) -> Result<Token, LexerError> {
+    fn scan_number_token(&mut self, first: char) -> Result<Token, LexerError> {
         let mut value = String::new();
         let mut times = 0;
         value.push(first);
@@ -126,7 +126,7 @@ impl<'a> Lexer<'a> {
         Err(LexerError::not_exist_terminal_symbol())
     }
 
-    fn parse_bool_token(&mut self, expect_bool: bool, index: usize) -> Result<Token, LexerError> {
+    fn scan_bool_token(&mut self, expect_bool: bool, index: usize) -> Result<Token, LexerError> {
         let s: String;
         let (s, end) = if expect_bool {
             // すでに最初の`t`は消費されている前提なので残り文字を精査
@@ -145,7 +145,7 @@ impl<'a> Lexer<'a> {
         }
     }
 
-    fn parse_null_token(&mut self, index: usize) -> Result<Token, LexerError> {
+    fn scan_null_token(&mut self, index: usize) -> Result<Token, LexerError> {
         // `null`かどうか文字を取得
         let s = "n".to_string() + &self.take_chars_with(3);
         let location = Location(index, index + 3);
@@ -156,7 +156,7 @@ impl<'a> Lexer<'a> {
         }
     }
 
-    fn parse_comment_token(&mut self) -> Result<Token, LexerError> {
+    fn scan_comment_token(&mut self) -> Result<Token, LexerError> {
         let (second_slash, next_char) = self
             .input
             .next()
@@ -216,7 +216,7 @@ impl<'a> Lexer<'a> {
         Err(LexerError::not_exist_terminal_symbol())
     }
 
-    fn parse_whitespaces(&mut self) -> Result<Token, LexerError> {
+    fn scan_whitespaces(&mut self) -> Result<Token, LexerError> {
         let mut length: usize = 1; // 呼び出し時点で1
         while let Some((index, c)) = self.input.peek() {
             let c = *c;
@@ -258,7 +258,7 @@ mod tests {
     }
 
     #[test]
-    fn lexer_should_success_parse() {
+    fn lexer_should_success_scan() {
         let mut lexer = Lexer::new(
             "{
     \"name\": \"sato\",\
@@ -311,23 +311,23 @@ mod tests {
     }
 
     #[test]
-    fn parse_string_token_should_return_token() {
+    fn scan_string_token_should_return_token() {
         let mut lexer = Lexer::new("name123\"");
         let token = lexer
-            .parse_string_token()
-            .expect("[parse_string_token_should_return_token]\"name\"のparseに失敗しました。");
+            .scan_string_token()
+            .expect("[scan_string_token_should_return_token]\"name\"のscanに失敗しました。");
         assert_eq!(Token::string("name123", Location(0, 7)), token);
 
         let mut lexer = Lexer::new("あいうえお\"");
-        let token = lexer.parse_string_token().expect(
-            "[parse_string_token_should_return_token]\"あいうえお\"のparseに失敗しました。",
+        let token = lexer.scan_string_token().expect(
+            "[scan_string_token_should_return_token]\"あいうえお\"のscanに失敗しました。",
         );
         assert_eq!(Token::string("あいうえお", Location(0, 5)), token);
 
         let mut lexer = Lexer::new(r#"\u3042\u3044\u3046abc""#);
         let token = lexer
-            .parse_string_token()
-            .expect("[parse_string_token_should_return_token]\"あいうabc\"のparseに失敗しました。");
+            .scan_string_token()
+            .expect("[scan_string_token_should_return_token]\"あいうabc\"のscanに失敗しました。");
         assert_eq!(
             Token::string("\\u3042\\u3044\\u3046abc", Location(0, 21)),
             token
@@ -335,8 +335,8 @@ mod tests {
 
         let mut lexer = Lexer::new(r#"\ud83d\ude00\ud83d\udc4d""#);
         let token = lexer
-            .parse_string_token()
-            .expect("[parse_string_token_should_return_token]\"😀👍\"のparseに失敗しました。");
+            .scan_string_token()
+            .expect("[scan_string_token_should_return_token]\"😀👍\"のscanに失敗しました。");
         assert_eq!(
             Token::string("\\ud83d\\ude00\\ud83d\\udc4d", Location(0, 24)),
             token
@@ -344,14 +344,14 @@ mod tests {
 
         let mut lexer = Lexer::new("😀👍\"");
         let token = lexer
-            .parse_string_token()
-            .expect("[parse_string_token_should_return_token]\"😀👍\"のparseに失敗しました。");
+            .scan_string_token()
+            .expect("[scan_string_token_should_return_token]\"😀👍\"のscanに失敗しました。");
         assert_eq!(Token::string("😀👍", Location(0, 2)), token);
 
         let mut lexer = Lexer::new(r#"test\"\/\\\b\n\f\r\t""#);
         let token = lexer
-            .parse_string_token()
-            .expect(r#"[parse_string_token_should_return_token]"test\"\/\\\b\n\f\r\t""のparseに失敗しました。"#);
+            .scan_string_token()
+            .expect(r#"[scan_string_token_should_return_token]"test\"\/\\\b\n\f\r\t""のscanに失敗しました。"#);
         assert_eq!(
             Token::string(r#"test\"\/\\\b\n\f\r\t"#, Location(0, 20)),
             token
@@ -359,124 +359,124 @@ mod tests {
     }
 
     #[test]
-    fn parse_string_token_should_err() {
+    fn scan_string_token_should_err() {
         // 部分的なテストのためのinvalid json
         let mut lexer = Lexer::new("name");
-        assert!(lexer.parse_string_token().is_err());
+        assert!(lexer.scan_string_token().is_err());
     }
 
     #[test]
-    fn parse_number_token_should_return_token() {
+    fn scan_number_token_should_return_token() {
         // 部分的なテストのためのinvalid json
         let mut lexer = Lexer::new(":100,");
         // 最初の`"`まで進める
         lexer.input.next();
         let (_, first) = lexer.input.next().unwrap();
-        if let Ok(token) = lexer.parse_number_token(first) {
+        if let Ok(token) = lexer.scan_number_token(first) {
             assert_eq!(Token::number("100", Location(2, 4)), token);
         } else {
-            panic!("[parse_string_token]がErrを返しました。");
+            panic!("[scan_string_token]がErrを返しました。");
         };
     }
 
     #[test]
-    fn parse_number_token_should_err() {
+    fn scan_number_token_should_err() {
         // 部分的なテストのためのinvalid json
         let mut lexer = Lexer::new(":100");
         // 最初の`"`まで進める
         lexer.input.next();
         let (_, first) = lexer.input.next().unwrap();
-        assert!(lexer.parse_number_token(first).is_err());
+        assert!(lexer.scan_number_token(first).is_err());
     }
 
     #[test]
-    fn parse_bool_token_should_return_true_token() {
+    fn scan_bool_token_should_return_true_token() {
         // 部分的なテストのためのinvalid json
         let mut lexer = Lexer::new(":true,");
         // 最初の`t`まで進める
         lexer.input.next();
         let (index, _) = lexer.input.next().unwrap();
-        if let Ok(token) = lexer.parse_bool_token(true, index) {
+        if let Ok(token) = lexer.scan_bool_token(true, index) {
             assert_eq!(Token::boolean(true, Location(1, 4)), token);
         } else {
-            panic!("[parse_string_token]がErrを返しました。");
+            panic!("[scan_string_token]がErrを返しました。");
         };
     }
 
     #[test]
-    fn parse_bool_token_should_err_with_true() {
+    fn scan_bool_token_should_err_with_true() {
         // 部分的なテストのためのinvalid json
         let mut lexer = Lexer::new(":tru");
         // 最初の`t`まで進める
         lexer.input.next();
         let (index, _) = lexer.input.next().unwrap();
-        assert!(lexer.parse_bool_token(true, index).is_err());
+        assert!(lexer.scan_bool_token(true, index).is_err());
     }
 
     #[test]
-    fn parse_bool_token_should_return_false_token() {
+    fn scan_bool_token_should_return_false_token() {
         // 部分的なテストのためのinvalid json
         let mut lexer = Lexer::new(":false,");
         // 最初の`f`まで進める
         lexer.input.next();
         let (index, _) = lexer.input.next().unwrap();
-        if let Ok(token) = lexer.parse_bool_token(false, index) {
+        if let Ok(token) = lexer.scan_bool_token(false, index) {
             assert_eq!(Token::boolean(false, Location(1, 5)), token);
         } else {
-            panic!("[parse_bool_token]がErrを返しました。");
+            panic!("[scan_bool_token]がErrを返しました。");
         };
     }
 
     #[test]
-    fn parse_bool_token_should_err_with_false() {
+    fn scan_bool_token_should_err_with_false() {
         // 部分的なテストのためのinvalid json
         let mut lexer = Lexer::new(":fal");
         // 最初の`f`まで進める
         lexer.input.next();
         let (index, _) = lexer.input.next().unwrap();
-        assert!(lexer.parse_bool_token(false, index).is_err());
+        assert!(lexer.scan_bool_token(false, index).is_err());
     }
 
     #[test]
-    fn parse_null_token_should_return_token() {
+    fn scan_null_token_should_return_token() {
         // 部分的なテストのためのinvalid json
         let mut lexer = Lexer::new(":null,");
         // 最初の`f`まで進める
         lexer.input.next();
         let (index, _) = lexer.input.next().unwrap();
-        if let Ok(token) = lexer.parse_null_token(index) {
+        if let Ok(token) = lexer.scan_null_token(index) {
             assert_eq!(Token::null(Location(1, 4)), token);
         } else {
-            panic!("[parse_null_token]がErrを返しました。");
+            panic!("[scan_null_token]がErrを返しました。");
         };
     }
 
     #[test]
-    fn parse_null_token_should_err() {
+    fn scan_null_token_should_err() {
         // 部分的なテストのためのinvalid json
         let mut lexer = Lexer::new(":nu");
         // 最初の`n`まで進める
         lexer.input.next();
         let (index, _) = lexer.input.next().unwrap();
-        assert!(lexer.parse_null_token(index).is_err());
+        assert!(lexer.scan_null_token(index).is_err());
     }
 
     #[test]
-    fn parse_comment_line_token_should_return_token() {
+    fn scan_comment_line_token_should_return_token() {
         // 部分的なテストのためのinvalid json
         let mut lexer = Lexer::new(",// comment \n}");
         // 最初の`/`まで進める
         lexer.input.next();
         lexer.input.next();
-        if let Ok(token) = lexer.parse_comment_token() {
+        if let Ok(token) = lexer.scan_comment_token() {
             assert_eq!(Token::comment_line(" comment ", Location(1, 12)), token);
         } else {
-            panic!("[parse_comment_token]がErrを返しました。");
+            panic!("[scan_comment_token]がErrを返しました。");
         };
     }
 
     #[test]
-    fn parse_comment_block_token_should_return_token() {
+    fn scan_comment_block_token_should_return_token() {
         // 部分的なテストのためのinvalid json
         let mut lexer = Lexer::new(
             r#"/*
@@ -487,7 +487,7 @@ test comment
         );
         // 最初の`/`まで進める
         lexer.input.next();
-        if let Ok(token) = lexer.parse_comment_token() {
+        if let Ok(token) = lexer.scan_comment_token() {
             assert_eq!(
                 Token::comment_block(
                     r#"
@@ -500,28 +500,28 @@ test comment
                 token
             );
         } else {
-            panic!("[parse_comment_token]がErrを返しました。");
+            panic!("[scan_comment_token]がErrを返しました。");
         };
     }
 
     #[test]
-    fn parse_comment_token_should_err() {
+    fn scan_comment_token_should_err() {
         // 部分的なテストのためのinvalid json
         let mut lexer = Lexer::new("/,");
         lexer.input.next().unwrap();
-        assert!(lexer.parse_comment_token().is_err());
+        assert!(lexer.scan_comment_token().is_err());
     }
 
     #[test]
-    fn parse_whitespaces_token_should_return_token() {
+    fn scan_whitespaces_token_should_return_token() {
         // 部分的なテストのためのinvalid json
         let mut lexer = Lexer::new(r#"   ""#);
         // 最初の` `まで進める
         lexer.input.next();
-        if let Ok(token) = lexer.parse_whitespaces() {
+        if let Ok(token) = lexer.scan_whitespaces() {
             assert_eq!(Token::white_spaces(3, Location(0, 3)), token);
         } else {
-            panic!("[parse_comment_token]がErrを返しました。");
+            panic!("[scan_comment_token]がErrを返しました。");
         };
     }
 }
